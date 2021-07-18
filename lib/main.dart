63d900c315e714bdf8583729a5c9f9e7d32f3c77
Giovanni Dionisi <dionisigiovanni@hotmail.com>
2021-07-19 00:33:26 +0200

    comprensione dei metodi con i bottoni (con errori)

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,57 @@ import 'package:flutter/material.dart';
 //chiamiamo il metodo main con la notazione 'arrow function'
 void main() => runApp(MyApp());
 
+int questionIndex = 0;
+
+void rispostaAllaDomanda() {
+  questionIndex = questionIndex + 1;
+  print(questionIndex);
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-            backgroundColor: Color(0xffC19556),
-          ),
-          body: Center(
-            child: Body(),
-          )),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('La mia prima applicazione in Flutter'),
+              backgroundColor: Color(0xffC19556),
+            ),
+            body: Center(
+              child: Body(),
+            )));
   }
 }
 
 class Body extends StatelessWidget {
+  final List<String> questions = [
+    'Qual\'è il tuo colore preferito?',
+    'Qual\'è il tuo animale preferito?'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Text('This is my default text!');
+    return Column(
+      children: [
+        Text(questions[questionIndex]),
+        RaisedButton(
+          child: Text('Risposta 1'),
+          onPressed: () {
+            // ...
+            int a = 2;
+            int b = 5;
+            print(a + b);
+          },
+        ),
+        RaisedButton(
+          child: Text('Risposta 2'),
+          onPressed: () => rispostaAllaDomanda,
+        ),
+        RaisedButton(
+          child: Text('Risposta 3'),
+          onPressed: () => rispostaAllaDomanda,
+        ),
+      ],
+    );
   }
 }
