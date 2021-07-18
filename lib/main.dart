690ba9bec1a409ee87c899fb89996491d4bc920e
Giovanni Dionisi <dionisigiovanni@hotmail.com>
2021-07-18 19:59:18 +0200

    avvio app base + metodo main in arrow function

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-//prima funzione eseguita quando l'app viene lanciata
-void main() {
-  runApp(MyApp());
-}
+//funzione eseguita quando l'app viene lanciata
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+//chiamiamo il metodo main con la notazione 'arrow function'
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello!'),
