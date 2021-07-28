b0fcb7d38791ddf64d223493e2846d9ac6cccc04
Giovanni Dionisi <dionisigiovanni@hotmail.com>
2021-07-29 01:03:31 +0200

    divisione dell'app in più widgets (main.dart splittato in differenti file, con differenti porizioni di codice).

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 //funzione eseguita quando l'app viene lanciata
 
@@ -20,6 +20,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // si utilizza la definizione della variabile 'final' se un valore non cambia dal momento in cui viene eseguito il programma.
+  //è perciò un valore costante di runtime.
+  final _questions = const [
+    //questa è la sintassi per inizializzare una 'map' che può essere anche inizializzata con Map()
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Monkey', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favorite football player?',
+      'answers': ['Messi', 'Ronaldo', 'Mbappe', 'Ronaldinho'],
+    },
+  ];
   int _questionIndex = 0;
 
   void _answerQuestion() {
@@ -29,51 +46,28 @@ class _MyAppState extends State<MyApp> {
     });
 
     print(_questionIndex);
+    print('oh yes!!!everything work fine!!!');
+    if (_questionIndex < _questions.length) {
+    } else {
+      print('oh no!!! we have more questions!!!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // si utilizza la definizione della variabile 'final' se un valore non cambia dal momento in cui viene eseguito il programma.
-    //è perciò un valore costante di runtime.
-    final questions = [
-      //questa è la sintassi per inizializzare una 'map' che può essere anche inizializzata con Map()
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Monkey', 'Lion'],
-      },
-      {
-        'questionText': 'Who\'s your favorite football player?',
-        'answers': ['Messi', 'Ronaldo', 'Mbappe', 'Ronaldinho'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-          // backgroundColor: Color(0xffC19556),
-          backgroundColor: Colors.blue,
-        ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            //'...' spred operatore - questo operatore di diffusione quello che fà è: prendere un elenco, estrae tutti i valori di quell'elenco e li aggiunge
-            // all'elenco circostante come valori individuali in modo da non aggiungere una lista nell'elenco.
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-            //NB: map metodo non cambia l'elenco originale delle domande. Genera invece una nuova lista basata sulla vecchia lista e quindi la trasformazione che facciamo
-            //nel corpo la passiamo al metodo map.
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+            // backgroundColor: Color(0xffC19556),
+            backgroundColor: Colors.blue,
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions)
+              : Result()),
     );
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  Quiz(
+      {@required this.questions,
+      @required this.questionIndex,
+      @required this.answerQuestion});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]['questionText'],
+        ),
+        //'...' spred operatore - questo operatore di diffusione quello che fà è: prendere un elenco, estrae tutti i valori di quell'elenco e li aggiunge
+        // all'elenco circostante come valori individuali in modo da non aggiungere una lista nell'elenco.
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(answerQuestion, answer);
+        }).toList(),
+        //NB: map metodo non cambia l'elenco originale delle domande. Genera invece una nuova lista basata sulla vecchia lista e quindi la trasformazione che facciamo
+        //nel corpo la passiamo al metodo map.
+      ],
+    );
+  }
+}
