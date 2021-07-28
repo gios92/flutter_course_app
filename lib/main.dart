c2a8f9156387081158972038e53badef0c4b54a5
Giovanni Dionisi <dionisigiovanni@hotmail.com>
2021-07-29 00:02:52 +0200

    introdotto il concetto di map (chiave:valore).

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
+    // si utilizza la definizione della variabile 'final' se un valore non cambia dal momento in cui viene eseguito il programma.
+    //è perciò un valore costante di runtime.
+    final questions = [
+      //questa è la sintassi per inizializzare una 'map' che può essere anche inizializzata con Map()
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Monkey', 'Lion'],
+      },
+      {
+        'questionText': 'Who\'s your favorite football player?',
+        'answers': ['Messi', 'Ronaldo', 'Mbappe', 'Ronaldinho'],
+      },
     ];
 
     return MaterialApp(
@@ -52,11 +61,16 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            //'...' spred operatore - questo operatore di diffusione quello che fà è: prendere un elenco, estrae tutti i valori di quell'elenco e li aggiunge
+            // all'elenco circostante come valori individuali in modo da non aggiungere una lista nell'elenco.
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList(),
+            //NB: map metodo non cambia l'elenco originale delle domande. Genera invece una nuova lista basata sulla vecchia lista e quindi la trasformazione che facciamo
+            //nel corpo la passiamo al metodo map.
           ],
         ),
       ),
